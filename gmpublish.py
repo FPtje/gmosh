d91fb95875630474aea271041bdfe53c4725b763
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-03-16 17:49:01 +0100

    Added gmpublish and gmad modules

---
+++ gmpublish.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class GmPublish:
+	"""Wrapper for the GMPublish program"""
+	
+	def __init__(self, addon):
+		this.addon = addon
+
--- wosh.py
+++ wosh.py
@@ -2,15 +2,18 @@
 import argparse
 import os
 import addoninfo
+from gmpublish import GmPublish
 
 def main():
 	# Define command line parameters
 	parser = argparse.ArgumentParser(description = "Garry's mod workshop cli wrapper.")
 	args = parser.parse_args()
+	curdir = os.getcwd()
 
 	# Try to get the addon information
 	try:
-		addon = addoninfo.get_addon_info(addoninfo.find_addon(os.getcwd()))
+		addon = addoninfo.get_addon_info(addoninfo.find_addon(curdir))
+		publisher = GmPublish(addon)
 	except addoninfo.AddonNotFoundError as err:
 		print(err)
 		return
