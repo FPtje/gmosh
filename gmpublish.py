538204db8c0144725c55d086efef8246570b444b
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-03-29 18:07:15 +0100

    Retrieve the OS specific executable

--- gmpublish.py
+++ gmpublish.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 
 class GmPublish:
 	"""Wrapper for the GMPublish program"""
@@ -6,7 +7,7 @@ class GmPublish:
 	def __init__(self, addon):
 		self.addon = addon
 
-	def create(self):
+	def create(self, logo):
 		"""Upload to the workshop as a new addon"""
 		pass
 
@@ -14,3 +15,14 @@ class GmPublish:
 		"""Push an update of the addon to the workshop"""
 		message = message or self.addon.getdefault_changelog()
 
+	def _get_executable(self):
+		"""Retrieve the path of the gmpublish executable"""
+		platform = sys.platform
+		if platform == 'linux':
+			return 'gmpublish_linux'
+		elif platform == 'windows':
+			return 'gmpublish.exe'
+		elif platform == 'darwin': #untested
+			return 'gmpublish_osx'
+
+		raise NameError("Could not find executable for platform " + platform)
