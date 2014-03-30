ee0904987b30ae1ccad8cc6e1ffe607fa8b76d05
FPtje <fpeijnenburg@gmail.com>
2014-03-30 14:39:19 +0200

    Install guide for Windows

--- package/Windows/uninstall.bat
+++ package/Windows/uninstall.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
+
+:: Remove gmosh' directory in program files
 rmdir "%ProgramFiles%\gmosh" /Q /S
+:: Remove the path from the PATH variable
 pathed.exe -r "%ProgramFiles%\gmosh\bin"
 
 echo Uninstall complete
