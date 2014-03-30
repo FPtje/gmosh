90e001e7deb92950c6df54694d82a314fa4b8ddd
FPtje <fpeijnenburg@gmail.com>
2014-03-30 13:04:51 +0200

    Git bash compatibility

--- compile_windows.bat
+++ compile_windows.bat
@@ -1,2 +1,2 @@
 mkdir bin
-C:\Python33\Scripts\cxfreeze src\gmosh.py --target-dir=bin --include-modules=addoninfo,gmafile,gmpublish
+C:/Python33/Scripts/cxfreeze src/gmosh.py --target-dir=bin --include-modules=addoninfo,gmafile,gmpublish
