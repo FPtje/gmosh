d55f9d6dc363854e130fed75105331bcf660ad46
FPtje <fpeijnenburg@gmail.com>
2014-03-30 14:33:01 +0200

    Compile windows now makes the package

--- compile_windows.bat
+++ compile_windows.bat
@@ -1,2 +1,11 @@
-mkdir bin
-C:/Python33/Scripts/cxfreeze src/gmosh.py --target-dir=bin --include-modules=addoninfo,gmafile,gmpublish
+mkdir bin > NUL
+mkdir package\Windows\bin > NUL
+mkdir package\Windows\required > NUL
+
+CALL C:/Python33/Scripts/cxfreeze src/gmosh.py --target-dir=bin --include-modules=addoninfo,gmafile,gmpublish
+
+xcopy bin\gmosh.exe package\Windows\bin\ /y
+xcopy README.txt package\Windows\ /y
+xcopy required\gmpublish.exe package\Windows\required\ /y
+xcopy required\steam_api.dll package\Windows\required\ /y
+xcopy required\steam_appid.txt package\Windows\required\ /y
